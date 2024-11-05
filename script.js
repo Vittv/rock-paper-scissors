9f25a9671c06598bee0b70bb16e063dfadd3e541
Vittv <166668150+Vittv@users.noreply.github.com>
2024-11-05 00:57:53 -0300

    Write computer logic

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-console.log('working');
+let computerChoice = getRandomInt(3);
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+function getComputerChoice(computerChoice) {
+    let choice;
+
+    if (computerChoice == 0) {
+        choice = "Rock";
+    }
+
+    else if (computerChoice == 1) {
+        choice = "Paper";
+    }
+
+    else {
+        choice = "Scissors";
+    }
+
+    console.log(`I chose ${choice}!`);
+}
+
+getComputerChoice(computerChoice);
+
+
