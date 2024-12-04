06b6c6e66a69c12641df6a72c9bc77742c5e8381
Vittv <166668150+Vittv@users.noreply.github.com>
2024-12-04 17:36:22 -0300

    Finish initial logic with button implementation

--- script.js
+++ script.js
@@ -1,3 +1,100 @@
+let playerScore = 0;
+let charlotteScore = 0;
+let playerChoice = "";
+let charlotteChoice = "";
+
+const rockButton = document.querySelector(".rock-btn");
+const paperButton = document.querySelector(".paper-btn");
+const scissorsButton = document.querySelector(".scissors-btn");
+
+// Buttons will save the Player's choice and play a round
+rockButton.addEventListener("click", () => {
+    getPlayerChoice(".rock-btn")
+    playRound();
+});
+paperButton.addEventListener("click", () => {
+    getPlayerChoice(".paper-btn")
+    playRound();
+});
+scissorsButton.addEventListener("click", () => {
+    getPlayerChoice(".scissors-btn")
+    playRound();
+});
+
+// Generate 3 random numbers
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+};
+
+// Gathers the Player's choice and returns its value
+function getPlayerChoice(buttonClass) {
+    if (buttonClass === ".rock-btn") {
+        playerChoice = "Rock";
+        console.log(`You chose ${playerChoice}`)
+        return playerChoice        
+    }
+    else if (buttonClass === ".paper-btn") {
+        playerChoice = "Paper";
+        console.log(`You chose ${playerChoice}`)
+        return playerChoice      
+    }
+    else if (buttonClass === ".scissors-btn") {
+        playerChoice = "Scissors";
+        console.log(`You chose ${playerChoice}`)
+        return playerChoice      
+    };
+};
+
+// Gathers Charlotte's choice and returns its value
+function getCharlotteChoice() {
+    let charlotteNumber = getRandomInt(3);
+    if (charlotteNumber == 0) {
+        charlotteChoice = "Rock";
+        console.log(`Charlotte chose ${charlotteChoice}`)
+        return charlotteChoice
+    }
+    else if (charlotteNumber == 1) {
+        charlotteChoice = "Paper";
+        console.log(`Charlotte chose ${charlotteChoice}`)
+        return charlotteChoice
+    }
+    else if (charlotteNumber == 2) {
+        charlotteChoice = "Scissors";
+        console.log(`Charlotte chose ${charlotteChoice}`)
+        return charlotteChoice
+    };
+};
+
+// Handles the round's logic and returns its outcome
+function getRoundResult() {
+    getCharlotteChoice();
+
+    if (playerChoice === charlotteChoice) {
+        console.log("It's a tie")
+    } else if (
+        (playerChoice === "Rock" && charlotteChoice === "Scissors") ||
+        (playerChoice === "Paper" && charlotteChoice === "Rock") ||
+        (playerChoice === "Scissors" && charlotteChoice === "Paper")
+    ) { 
+        console.log("You win");
+        playerScore++; // +1 player score          
+    } else {
+        console.log("Charlotte wins");
+        charlotteScore++; // + 1 charlotte score
+    };
+};
+
+// Plays a round
+function playRound() {
+    getRoundResult();
+};
+
+function handleAllHTML() {
+
+};
+
+/* CLI version code, we'll keep it here as a reference for the time being
+
 let humanScore = 0;
 let computerScore = 0;
 
@@ -90,3 +187,4 @@ function game() {
 }
 
 game();
+*/
